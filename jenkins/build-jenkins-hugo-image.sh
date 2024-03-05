b86492eec2e5dec61a130d0439b9206a0cda415f
maishuren <maisrcn@qq.com>
2024-03-05 18:39:40 +0800

    1. 添加jenkins 2. 编写基于jenkins镜像，构建含有hugo环境的jenkins 3. 使用本地的ssh(如不需要，可以删掉/注释)

---
+++ jenkins/build-jenkins-hugo-image.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+jdk=$1
+if test -d ./docker; then
+  echo "docker folder already exist."
+else
+  git clone https://github.com/jenkinsci/docker.git
+fi
+
+cd docker && rm -rf Dockerfile
+
+if [[ $jdk = '17' ]]; then
+  ehco 'use debian jdk17'
+  cp 17/debian/bookworm/hotspot/Dockerfile Dockerfile
+elif [[ $jdk = '11' ]]; then
+  echo 'use debian jdk11'
+  cp 11/debian/bookworm/hotspot/Dockerfile Dockerfile
+elif [[ $jdk = '21' ]]; then
+  echo 'use debian jdk21'
+  cp 21/debian/bookworm/hotspot/Dockerfile Dockerfile
+else
+  echo 'usage build-jenkins-hugo.sh 11/17/21'
+fi
+# macos 上使用gsed, linux 上使用sed
+gsed -i '/ENV LANG C.UTF-8/i\apt-get install -y hugo' Dockerfile
+#sed -i '/ENV LANG C.UTF-8/i\apt-get install -y hugo' Dockerfile
+docker build . -t jenkins-hugo:latest
+
+echo "build done."
+
